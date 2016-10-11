453c6cf1e5919ac5a9eb7699de9bc345257419c7
Cole <league@WTS5.attlocal.net>
2016-10-11 16:57:26 -0700

    Shiny Objects!

---
+++ src/Bieber_Fever.java
@@ -0,0 +1,15 @@
+
+public class Bieber_Fever {
+	public static void main(String[] args) {
+		Baby("Oh \n");
+		Baby("No \n");
+		Baby("Oh \n");
+		System.out.print("I THOUGHT YOU'D ALWAYS BE MINE.");
+	}
+public static void Baby(String Oh){
+	for (int laser=1; laser<=3;laser++){
+		System.out.print("BABY ");
+}
+	System.out.print(Oh);
+}
+}
---
+++ src/ShinyObjects.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class ShinyObjects {
+
+	public static void main(String[] args) {
+
+		// 2. Ask the user how many shiny objects they want
+		String shiny=JOptionPane.showInputDialog("How many shiny objects would you like?");
+		// 3. Play the sound that many times
+       int shing=Integer.parseInt(shiny);
+       for (int shinging= 0; shinging <= shing; shinging++){
+		// 1. Call the method below
+		playMisterZee();
+       }
+	}
+
+	public static void playMisterZee() {
+    	try {
+   		 File soundFile = new File("/Users/League/Google Drive/league-sounds/shiny-objects.wav");
+   		 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+   		 Clip clip = AudioSystem.getClip();
+   		 clip.open(audioInputStream);
+   		 clip.start();
+   		 Thread.sleep(3400);
+    	} catch (Exception ex) {
+        	ex.printStackTrace();
+    	}
+	}
+
+}
+
